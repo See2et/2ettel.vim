65d023590970775cdee25acd73633c941b866b83
see2et <seezet1024@gmail.com>
2024-02-29 22:51:01 +0900

    uo

--- denops/2ettel.vim/main.ts
+++ denops/2ettel.vim/main.ts
@@ -1,7 +1,7 @@
 import { Denops, parse } from "./deps.ts";
 import { setCmd } from "./lib/setCmd.ts";
 
-export let dir = "";
+export let dir = "~/2ettel";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
@@ -15,8 +15,15 @@ export async function main(denops: Denops): Promise<void> {
         ? await denops.cmd(`echo "Please do :ZettelSetDir <dir>"`)
         : await denops.cmd(`echo "Your memos are saved in ${dir}"`);
     },
+    async createMemo(args: unknown) {
+      const [, , residues] = parse(args as string[]);
+      const title = residues.join("");
+      Deno.createSync(`${dir}/${title}.md`);
+      await denops.cmd(`echo "created ${dir}/${title}.md"`);
+    },
   };
 
   setCmd(denops, "ZettelSetDir", "setDir");
   setCmd(denops, "ZettelGetDir", "getDir");
+  setCmd(denops, "ZettelMemo", "createMemo");
 }
